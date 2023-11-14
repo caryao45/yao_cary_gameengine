2b0505dc037ac8836087bd3ee3be9e1486273bdd
Cary Yao <142251248+caryao45@users.noreply.github.com>
2023-11-14 13:59:08 -0800

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -1,31 +1,36 @@
+from typing import Any
 import pygame as pg
 from pygame.sprite import Sprite
+from random import randint
+import math
 
 from pygame.math import Vector2 as vec
 import os
 from settings import *
 
-# setup asset folders here
+# setup asset folders here - images sounds etc.
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, 'images')
 snd_folder = os.path.join(game_folder, 'sounds')
 
-class Player(pg.sprite.Sprite):
-    # defines function that prints image in pygame 
+# player class
+class Player(Sprite):
     def __init__(self, game):
         Sprite.__init__(self)
         # self.image = pg.Surface((50, 50))
         # self.image.fill(GREEN)
+        # use an image for player sprite...
         self.game = game
-        self.image = pg.image.load(os.path.join(img_folder, 'theBell.png')).convert()
+        self.image = pg.image.load(os.path.join(img_folder, 'theBigBell.png')).convert()
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH/2, HEIGHT/2)
-        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.rect.center = (0, 0)
+        self.pos = vec(WIDTH/2, 680)
         self.vel = vec(0,0)
         self.acc = vec(0,0)
-        print(self.rect.center)
-    # define function controls that moves image when pressing wasd
+        self.score = 0
+        self.hitpoints = 50
+    # defines controls for player
     def controls(self):
         keys = pg.key.get_pressed()
         if keys[pg.K_a]:
@@ -34,82 +39,55 @@ class Player(pg.sprite.Sprite):
             self.acc.x = 5
         if keys[pg.K_SPACE]:
             self.jump()
+        # reset button after game over; couldn't figure out how to respawn coins
+        if keys[pg.K_r]:
+            self.score = 0
+            self.hitpoints = 50
+            self.pos = vec(WIDTH/2, 680)
     def jump(self):
         hits = pg.sprite.spritecollide(self, self.game.all_platforms, False)
         if hits:
             # print("i can jump")
             self.vel.y = -PLAYER_JUMP
-    # define function update that updates the image when the controls are used and if the image collides with somethingx
     def update(self):
-        # self.rect.x += 5
-        # self.rect.y += 5
+        # CHECKING FOR COLLISION WITH MOBS HERE>>>>>
         self.acc = vec(0,PLAYER_GRAV)
         self.controls()
-        # hits = pg.sprite.spritecollide(self, all_platforms, False)
-        # if hits:
-            # print("I've collided ...")
-        # print(all_platforms)
         # if friction - apply here
-        self.acc.x += self.vel.x * -0.2 
-        self.acc.y += self.vel.y * -0.2
+        self.acc.x += self.vel.x * -PLAYER_FRIC
+        # self.acc.y += self.vel.y * -0.3
         # equations of motion
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
-        if self.rect.x > WIDTH:
-            self.rect.x = 0
-        if self.rect.y > HEIGHT:
-            self.rect.y = 0
         self.rect.midbottom = self.pos
+        # sends player to other side when leaving the screen 
+        # got inspiration from this video: https://www.youtube.com/watch?v=QuM-jEQ7fAA&ab_channel=CodingWithRuss 
+        if self.rect.left < 0:
+            self.pos.x = WIDTH - self.rect.right
+        if self.rect.right > WIDTH:
+            self.pos.x = WIDTH - self.rect.left
 
-# platforms 
+# platforms
 
 class Platform(Sprite):
-    def __init__(self, x, y, w, h, kind):
+    def __init__(self, x, y, w, h, category):
         Sprite.__init__(self)
         self.image = pg.Surface((w, h))
         self.image.fill(GREEN)
-        # self.image = pg.image.load(os.path.join(img_folder, 'scissors.gif')).convert()
-        # self.image.set_colorkey(WHITE)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.kind = kind
-        # self.pos = vec(WIDTH/2, HEIGHT/2)
-        # self.vel = vec(0,0)
-        # self.acc = vec(0,0)
-        print(self.rect.center)
-
-# moving platform
-class MPlatform(Sprite):
-    def __init__(self, x, y, w, h, kind):
-        Sprite.__init__(self)
-        self.image = pg.Surface((w, h))
-        self.image.fill(GREEN)
-        # self.image = pg.image.load(os.path.join(img_folder, 'scissors.gif')).convert()
-        # self.image.set_colorkey(WHITE)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.kind = kind
-        self.vel = vec(0,0)
-        self.acc = vec(0,0)
-        print(self.rect.center)
+        self.category = category
+        self.speed = 0
+        if self.category == "moving":
+            self.speed = 5
     def update(self):
-        self.pos = self.rect.x
-        self.rect.x = self.pos + 2
+        if self.category == "moving":
+            self.rect.x += self.speed
+            if self.rect.x + self.rect.w > WIDTH or self.rect.x < 0:
+                self.speed = -self.speed
 
- # ice platform   
-class Ice_plat(Platform):
-    def __init__(self, x, y, w, h, kind):
-        Platform.__init__(self, x, y, w, h, kind)
-        self.image = pg.Surface((w, h))
-        self.image.fill(WHITE)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.kind = kind
-        print("i made an ice plat")
-    
+# mob class
 class Mob(Sprite):
     def __init__(self, x, y, w, h, kind):
         Sprite.__init__(self)
@@ -119,5 +97,22 @@ class Mob(Sprite):
         self.rect.x = x
         self.rect.y = y
         self.kind = kind
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+    
+    def update(self):
+        pass
+
+# coin class
+class Coin(Sprite):
+    def __init__(self, x, y, w, h, kind):
+        Sprite.__init__(self)
+        self.image = pg.image.load(os.path.join(img_folder, 'coin.png')).convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.kind = kind
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+
     def update(self):
         pass
